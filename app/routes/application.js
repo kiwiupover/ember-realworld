69e53e30662013446c303296dab58d45e2c2eca0
Dave Laird <kiwiupover@gmail.com>
2021-02-16 19:18:49 -0800

    make sure we are using ember-fetch in the session service

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,11 +3,8 @@ import { inject as service } from '@ember/service';
 
 export default class ApplicationRoute extends Route {
   @service session;
-  @service fastboot;
 
   model() {
-    if (this.fastboot.isFastBoot) return;
-
     return this.session.initSession();
   }
 }
